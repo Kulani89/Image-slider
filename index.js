695453de0adb1837e7873694c9e6ffe6bc606d93
kulani <h1l1zatkh@gmail.com>
2023-05-25 14:02:04 +0200

    JS setTimeout method

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const prevEl = document.querySelector(".prev");
 const nextEl = document.querySelector(".next");
 
-const sliderContainerEl = document.querySelector(".slider-container");
+// const sliderContainerEl = document.querySelector(".slider-container");
 const imageContainerEl = document.querySelector(".image-container")
 
-let curentImg = 1;
+const imgsEl = document.querySelectorAll("img");
+
+let currentImg = 1;
+let timeout;
 
 nextEl.addEventListener("click", () =>{
-    curentImg++
+    currentImg++
+    clearTimeout(timeout);
     updateImg();
-})
+});
+
+prevEl.addEventListener("click", () =>{
+    currentImg--
+    clearTimeout(timeout);
+    updateImg();
+});
+
+updateImg();
 
 function updateImg(){
-
+    if(currentImg > imgsEl.length){
+        currentImg = 1;
+    }else if(currentImg < 1){
+        currentImg = imgsEl.length;
+    }
+    imageContainerEl.style.transform = `translate(-${(currentImg - 1) * 500}px)`;
+   timeout = setTimeout(() =>{
+        currentImg++
+        updateImg();
+    }, 3000);
 }
